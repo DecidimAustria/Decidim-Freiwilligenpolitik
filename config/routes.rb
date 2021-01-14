fe1692252dc86e1a2cec824037d448443ea8a8b8
Alexander Rusa <alex@rusa.at>
2021-01-14 17:14:38 +0100

    Add sidekiq config

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  end
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
